1fd28eaefd0d75959b8cf5ac7f68c43e744acfd8
unknown <shaileshpanghate29816@gmail.com>
2025-02-25 15:13:14 +0530

    changes our work////

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Pages/Home'
-import About from './Pages/About'
-import Services from './Pages/Services'
 import Contact from './Pages/Contact'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -13,8 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/services" element={<Services />} />
         <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </Router>
